187b1015ca3d466342a4bd8cf62710741bf6e36b
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2021-07-24 17:35:45 -0300

    Some testing

--- urlPdfDownloader.py
+++ urlPdfDownloader.py
@@ -15,15 +15,22 @@ def get_pdf_files_from_url(url, first, last):
     return files
 
 
-get_pdf_files_from_url( "http://www.repositori.uji.es/xmlui/bitstream/handle/10234/5875/bolAuto",1,10)
+"""get_pdf_files_from_url( "https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/MIT6_006F11_lec0",0,)"""
 
-"""pdf_path = "http://www.repositori.uji.es/xmlui/bitstream/handle/10234/5875/bolAuto"
+
 def download_file(download_url, filename):
     response = urllib.request.urlopen(download_url)    
     file = open(filename + ".pdf", 'wb')
     print(type(file))
+    file.write(response.read())
+    file.close()
  
+pdf_path = "http://csg.csail.mit.edu/6.823/Lectures/L0"
 
-for i in range(7,100):
-    download_file(pdf_path+str(i)+".pdf", "bolAuto"+str(i)+".pdf")
-"""
+for i in range(1,10):
+    download_file(pdf_path+str(i)+"split.pdf", "L0"+str(i)+"split")
+
+pdf_path = "http://csg.csail.mit.edu/6.823/Lectures/L"
+
+for i in range(10,30):
+    download_file(pdf_path+str(i)+"split.pdf", "L"+str(i)+"split")
